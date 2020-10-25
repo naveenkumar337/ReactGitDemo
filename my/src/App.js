09677caea4fc93b48d3d6e5d88e8bdda1e6d932c
naveenkumar337 <naveenajay337@gmail.com>
2020-10-25 20:09:42 +0530

    first commit

---
+++ my/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Wel from './Welcome'
+
+function App() {
+  return (
+    <div className="App">
+     <Wel />
+    </div>
+  );
+}
+
+export default App;
---
+++ my/src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Welcome() {
+    return (
+        <div>
+            <h1>
+                Welcome to Git Session...
+            </h1>
+        </div>
+    )
+}
+
+export default Welcome
