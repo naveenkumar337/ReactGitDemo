2fad4ff6fb2d31499babf9a73ac59c92e55be38b
naveenkumar337 <naveenajay337@gmail.com>
2020-10-25 21:18:31 +0530

    chnages

--- my/src/App.js
+++ my/src/App.js
@@ -9,6 +9,10 @@ function App() {
      <h5>welcome to master</h5>
      <h4>Welcome to demo2</h4>
      <h3>welcome to git control</h3>
+     <ol>
+       <li>demo1</li>
+       <li>demo2</li>
+     </ol>
     </div>
   );
 }
